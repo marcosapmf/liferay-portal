5cad3f5fece7dec4841741c22faca9f9880a23fe
Marcos Martins <marcos.martins@liferay.com>
2022-03-04 17:09:35 -0300

    LRAC-10637 dxp entities batch engine task item delegates

---
+++ modules/apps/analytics/analytics-dxp-entities-exporter-impl/src/main/java/com/liferay/analytics/dxp/entities/exporter/internal/batch/v1_0/UserDXPEntityBatchEngineTaskItemDelegate.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.dxp.entities.exporter.internal.batch.v1_0;
+
+import com.liferay.analytics.dxp.entities.exporter.dto.v1_0.DXPEntity;
+import com.liferay.analytics.dxp.entities.exporter.internal.helper.DXPEntityBatchEngineTaskItemDelegateHelper;
+import com.liferay.analytics.dxp.entities.exporter.internal.odata.entity.v1_0.DXPEntityEntityModel;
+import com.liferay.batch.engine.BaseBatchEngineTaskItemDelegate;
+import com.liferay.batch.engine.BatchEngineTaskItemDelegate;
+import com.liferay.batch.engine.pagination.Page;
+import com.liferay.batch.engine.pagination.Pagination;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.filter.Filter;
+import com.liferay.portal.odata.entity.EntityModel;
+
+import java.io.Serializable;
+
+import java.util.List;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marcos Martins
+ */
+@Component(
+	enabled = true, immediate = true,
+	property = "batch.engine.task.item.delegate.name=user-dxp-entities",
+	service = BatchEngineTaskItemDelegate.class
+)
+public class UserDXPEntityBatchEngineTaskItemDelegate
+	extends BaseBatchEngineTaskItemDelegate<DXPEntity> {
+
+	@Override
+	public EntityModel getEntityModel(Map<String, List<String>> multivaluedMap)
+		throws Exception {
+
+		return new DXPEntityEntityModel();
+	}
+
+	@Override
+	public Page<DXPEntity> read(
+			Filter filter, Pagination pagination, Sort[] sorts,
+			Map<String, Serializable> parameters, String search)
+		throws Exception {
+
+		return _dxpEntityBatchEngineTaskItemDelegateHelper.getDXPEntitiesPage(
+			User.class.getName(), contextCompany.getCompanyId(), filter,
+			pagination, sorts, parameters, search);
+	}
+
+	@Reference
+	private DXPEntityBatchEngineTaskItemDelegateHelper
+		_dxpEntityBatchEngineTaskItemDelegateHelper;
+
+}
