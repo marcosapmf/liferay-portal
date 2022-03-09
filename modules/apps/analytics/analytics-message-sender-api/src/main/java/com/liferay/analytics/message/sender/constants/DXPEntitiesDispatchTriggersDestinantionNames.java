1f7c0eba3970841ef319ed8051f22f05fb83f048
Marcos Martins <marcos.martins@liferay.com>
2022-03-09 16:52:35 -0300

    LRAC-10637 create DXPEntitiesDispatchTriggersDestinantionNames

---
+++ modules/apps/analytics/analytics-message-sender-api/src/main/java/com/liferay/analytics/message/sender/constants/DXPEntitiesDispatchTriggersDestinantionNames.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.message.sender.constants;
+
+/**
+ * @author Marcos Martins
+ */
+public class DXPEntitiesDispatchTriggersDestinantionNames {
+
+	public static final String DXP_ENTITIES_DISPATCH_TRIGGER_PROCESSOR =
+		"liferay/dxp_entities_dispatch_trigger_processor";
+
+}
