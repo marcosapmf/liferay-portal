5d51d750655055d498e905fa8fa9c6ba8d40a138
Marcos Martins <marcos.martins@liferay.com>
2022-04-11 12:43:25 -0300

    LRAC-10824 user export should only be triggered when sync contacts is active

--- modules/apps/analytics/analytics-batch-export-import-impl/src/main/java/com/liferay/analytics/batch/exportimport/internal/dispatch/executor/BaseAnalyticsDXPEntityExportDispatchTaskExecutor.java
+++ modules/apps/analytics/analytics-batch-export-import-impl/src/main/java/com/liferay/analytics/batch/exportimport/internal/dispatch/executor/BaseAnalyticsDXPEntityExportDispatchTaskExecutor.java
@@ -45,6 +45,10 @@ public abstract class BaseAnalyticsDXPEntityExportDispatchTaskExecutor
 			DispatchTaskExecutorOutput dispatchTaskExecutorOutput)
 		throws Exception {
 
+		if (!shouldExport(dispatchTrigger.getCompanyId())) {
+			return;
+		}
+
 		DispatchLog dispatchLog =
 			dispatchLogLocalService.fetchLatestDispatchLog(
 				dispatchTrigger.getDispatchTriggerId(),
@@ -82,6 +86,10 @@ public abstract class BaseAnalyticsDXPEntityExportDispatchTaskExecutor
 	}
 
 	protected void postExecute(DispatchTrigger dispatchTrigger) {
+	}
+
+	protected boolean shouldExport(long companyId) {
+		return true;
 	}
 
 	@Reference
