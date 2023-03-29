1657406e3dc777b46ec5f992e23b6272a0d49c73
Marcos Martins <marcos.martins@liferay.com>
2023-03-29 12:06:57 -0300

    LRAC-11500 Migrate osb-faro-api

--- modules/dxp/apps/osb/osb-faro/osb-faro-api/src/main/java/com/liferay/osb/faro/util/EmailUtil.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-api/src/main/java/com/liferay/osb/faro/util/EmailUtil.java
@@ -17,31 +17,10 @@ package com.liferay.osb.faro.util;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Group;
 
-import java.util.Objects;
-
 /**
  * @author Matthew Kong
  */
 public class EmailUtil {
-
-	public static String getEmailBannerURL(String frequency) {
-		if (Objects.equals(frequency, "daily")) {
-			return _FARO_URL.concat(
-				"/o/osb-faro-web/images/email/ac-email-banner-daily.png");
-		}
-		else if (Objects.equals(frequency, "monthly")) {
-			return _FARO_URL.concat(
-				"/o/osb-faro-web/images/email/ac-email-banner-monthly.png");
-		}
-
-		return _FARO_URL.concat(
-			"/o/osb-faro-web/images/email/ac-email-banner-weekly.png");
-	}
-
-	public static String getLiferayLogoIconURL() {
-		return _FARO_URL.concat(
-			"/o/osb-faro-web/images/email/liferay-logo.png");
-	}
 
 	public static String getLogoIconURL() {
 		return _FARO_URL.concat("/o/osb-faro-web/images/email/ac-chart.png");
@@ -49,19 +28,6 @@ public class EmailUtil {
 
 	public static String getTitleIconURL() {
 		return _FARO_URL.concat("/o/osb-faro-web/images/email/ac-title.png");
-	}
-
-	public static String getTrendIconURL(String trend) {
-		if (Objects.equals(trend, "NEGATIVE")) {
-			return _FARO_URL.concat(
-				"/o/osb-faro-web/images/email/icon-order-arrow-down.png");
-		}
-		else if (Objects.equals(trend, "POSITIVE")) {
-			return _FARO_URL.concat(
-				"/o/osb-faro-web/images/email/icon-order-arrow-up.png");
-		}
-
-		return _FARO_URL.concat("/o/osb-faro-web/images/email/icon-empty.png");
 	}
 
 	public static String getWorkspaceURL(Group group) {
@@ -79,17 +45,6 @@ public class EmailUtil {
 			sb.append(StringPool.SLASH);
 			sb.append(group.getGroupId());
 		}
-
-		return sb.toString();
-	}
-
-	public static String getWorkspaceURL(String channelId, Group group) {
-		StringBuilder sb = new StringBuilder(4);
-
-		sb.append(getWorkspaceURL(group));
-		sb.append(StringPool.SLASH);
-		sb.append(channelId);
-		sb.append("/sites");
 
 		return sb.toString();
 	}
